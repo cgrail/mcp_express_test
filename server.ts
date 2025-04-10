802f7ff3f8ef518b41bb98ae0ee3335fd305934e
Christian Grail <cgrail@users.noreply.github.com>
2025-04-10 15:47:36 +0200

    Initial app

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Blub with Express!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
